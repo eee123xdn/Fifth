79d86c815f9b2f8d1d72d0a01390ef5edd6f31da
eee123xdn <69113660+eee123xdn@users.noreply.github.com>
2020-09-08 23:55:17 +0800

    Add files via upload

---
+++ startgame.h
@@ -0,0 +1,29 @@
+#ifndef STARTGAME_H
+#define STARTGAME_H
+#include<piggame.h>
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class StartGame; }
+QT_END_NAMESPACE
+extern int sum,day,money;
+class StartGame : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    StartGame(QWidget *parent = nullptr);
+    ~StartGame();
+
+private slots:
+    void on_btn_start_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+private:
+    Ui::StartGame *ui;
+};
+
+#endif // STARTGAME_H
